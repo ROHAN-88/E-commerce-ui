c1740ad7331fa54581350342c5c8035e6a0310d9
Rohan <therohanshrestha6655@gmail.com>
2023-10-18 18:43:41 +0545

    dot

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -80,6 +80,7 @@
 //   );
 // };
 //!================================= up is my code down in chatgtp code
+// Importing necessary parts from React and other libraries
 import React, { useEffect, useState } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
@@ -90,39 +91,56 @@ import { useQuery } from "react-query";
 import { lastestProduct } from "../../lib/product.api";
 import Loader from "../../Loader";
 
+// Creating a component named Home
 const Home = () => {
+  // State to hold data for different categories
   const [kitchenData, setKitchenData] = useState([]);
   const [electronicsData, setElectronicsData] = useState([]);
   const [clothingData, setClothingData] = useState([]);
+
+  // State to manage loading state
   const [isLoading, setIsLoading] = useState(true);
 
+  // Function to fetch products based on category
   const fetchProducts = async (category, setData) => {
     try {
+      // Trying to get product data from the server
       const response = await lastestProduct(10, { category });
+      // Updating the state with the received data
       setData(response.data);
     } catch (error) {
+      // If there's an error, log it to the console
       console.error("Error fetching products:", error);
     }
   };
 
+  // useEffect is like a helper that does something when the component is shown on the screen
   useEffect(() => {
+    // Inside useEffect, we're doing some tasks
     const fetchData = async () => {
+      // Setting loading to true to show a loading spinner
       setIsLoading(true);
 
-      // Fetch data for different categories
+      // Fetching data for different categories
       await fetchProducts("kitchen", setKitchenData);
       await fetchProducts("electronics", setElectronicsData);
       await fetchProducts("clothing", setClothingData);
 
+      // After fetching, setting loading to false to hide the spinner
       setIsLoading(false);
     };
 
+    // Calling the fetchData function
     fetchData();
+    // The empty array [] means this useEffect runs only once when the component is first shown
   }, []);
 
+  // Function to render OwlCarousel with HomeCardProduct items
   const renderCards = (data) => {
     return (
+      // Using OwlCarousel to create a nice sliding effect
       <OwlCarousel className="owl-theme" {...option}>
+        {/* Mapping through data and creating HomeCardProduct for each item */}
         {data.map((item) => (
           <HomeCardProduct key={item._id} {...item} className="item" />
         ))}
@@ -130,13 +148,14 @@ const Home = () => {
     );
   };
 
+  // Configuration options for OwlCarousel
   const option = {
     item: 2,
     loop: true,
     center: true,
     autoplay: false,
     nav: false,
-    dots: true,
+    dots: false,
     margin: 0,
     responsive: {
       600: {
@@ -145,14 +164,18 @@ const Home = () => {
     },
   };
 
+  // If still loading, show a loading spinner
   if (isLoading) {
     return <Loader />;
   }
 
+  // Once data is loaded, render the component
   return (
+    // Returning the JSX (HTML-like code) to be displayed on the screen
     <>
       <div>
         <div>
+          {/* Heading for the latest products */}
           <h1
             style={{
               marginLeft: "5rem",
@@ -177,6 +200,7 @@ const Home = () => {
           >
             Kitchen
           </h1>
+          {/* Render Kitchen category cards */}
           {renderCards(kitchenData)}
         </div>
 
@@ -192,6 +216,7 @@ const Home = () => {
           >
             Electronics
           </h1>
+          {/* Render Electronics category cards */}
           {renderCards(electronicsData)}
         </div>
 
@@ -207,6 +232,7 @@ const Home = () => {
           >
             Clothing
           </h1>
+          {/* Render Clothing category cards */}
           {renderCards(clothingData)}
         </div>
       </div>
@@ -214,4 +240,5 @@ const Home = () => {
   );
 };
 
+// Exporting the Home component to be used in other parts of the app
 export default Home;
